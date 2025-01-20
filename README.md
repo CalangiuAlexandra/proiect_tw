219a16121fe3d98eb6310e00ccecfceffc5140d2
Alexandra Calangiu <calangiualexandra22@stud.ase.ro>
2025-01-20 18:20:00 +0200

    Adaugat operatiuni pe entitati

--- README.md
+++ README.md
@@ -1,17 +1,28 @@
 Proiect TW
 
-BACKEND:
+- BACKEND:
 
-- Pentru a rula aplicatia se va naviga in 
-directorul "backend" si se vor rula comenzile:
+  - Pentru a rula aplicatia se va naviga in 
+  directorul "backend" si se vor rula comenzile:
 
-npm install
-node index.js
+          npm install
+          node index.js
 
-- Acestea vor instala dependintele proiectului
-si vor porni aplicatia pe portul 3001.
-- Am ales o baza de date sqlite pentru a stoca
-datele si Sequelize pentru ORM.
-- In aplicatie sunt prezente entitatile: Director,
-Movie si User cu relatii intru Movie si Director
-si User si Movie.
+  - Este indicat inainte de rularea comenzilor de mai sus
+  sa se stearga folderul "node_modules".
+  - Acestea vor instala dependintele proiectului
+  si vor porni aplicatia pe portul 3001.
+  - Am ales o baza de date sqlite pentru a stoca
+  datele si Sequelize pentru ORM.
+  - In aplicatie sunt prezente entitatile: Director,
+  Movie si User cu relatii intru Movie si Director
+  si User si Movie.
+
+  - Operatiuni:
+    - Toate operatiunile pe modele (adaugare, editare
+    , stergere, get) sunt definite in index.js din
+    folderul "backend".
+    - Pe langa operatiunile de baza exista si o operatiune
+    de adaugare a filmului la in lista de favorite.
+    - La get de utilizator vor fi trimise si filmele
+    adaugate de acesta la favorite.
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { sequelize, Director, Movie, User} = require('./models');
+const {genSaltSync, hashSync} = require("bcrypt");
 
 const app = express();
 
@@ -13,6 +14,280 @@ sequelize.sync({ force: true }).then(async () => {
     const user = await User.create({ username: 'alexandra', password: "alexandra" });
 
     await user.addMovie(movie);
+});
+
+const handleErrorResponse = (res, error, message) => {
+    console.error(`Error: ${message}`, error);
+    return res.status(500).json({ success: false, message: `Error ${message}.`
+    });
+};
+
+// Rute
+
+// Rutele pentru utilizatori
+app.get('/users', async (req, res) => {
+    try {
+        const users = await User.findAll({
+            include: [Movie],
+        });
+
+        res.status(200).json(users);
+    } catch (error) {
+        handleErrorResponse(res, error, 'Error retrieving users');
+    }
+});
+
+app.get('/users/:id', async (req, res) => {
+    try{
+        const user = await User.findByPk(req.params.id, {
+            include: [Movie],
+        });
+
+        if (!user) {
+            return res.status(404).json({ success: false, message: 'User not found.' });
+        }
+
+        res.status(200).json(user);
+    } catch (error) {
+        handleErrorResponse(res, error, 'Error retrieving user');
+    }
+});
+
+app.post('/users', async (req, res) => {
+    try {
+        const { username, password } = req.body; const salt = genSaltSync(10);
+        const hash = hashSync(password, salt);
+
+        const user = await User.create({
+            username: username,
+            password: hash,
+        });
+
+        delete user.dataValues.password;
+        res.status(201).json(user);
+    } catch (error) {
+        handleErrorResponse(res, error, 'Error creating user');
+    }
+});
+
+app.put('/users/:id', async (req, res) => {
+    const userId = req.params.id;
+    const updatedData = req.body;
+
+    try {
+        const user = await User.findByPk(userId);
+
+        if (!user) {
+            return res.status(404).json({ success: false, message: 'User notfound.' });
+        }
+
+        await user.update(updatedData);
+
+        return res.status(200).json({ success: true, message: 'User updated successfully.' });
+    } catch (error) {
+        handleErrorResponse(res, error, 'Error updating user');
+    }
+});
+
+app.delete('/users/:id', async (req, res) => {
+    const userId = req.params.id;
+
+    try {
+        const user = await User.findByPk(userId);
+        if (!user) {
+            return res.status(404).json({ success: false, message: 'User notfound.' });
+        }
+
+        await user.destroy();
+
+        return res.status(200).json({ success: true, message: 'User deletedsuccessfully.' });
+    } catch (error) {
+        handleErrorResponse(res, error, 'Error deleting user');
+    }
+});
+
+// Rutele pentru regizori
+app.get('/directors', async (req, res) => {
+    try {
+        const directors = await Director.findAll();
+
+        res.status(200).json(directors);
+    } catch (error) {
+        handleErrorResponse(res, error, 'Error retrieving directors');
+    }
+});
+
+app.get('/directors/:id', async (req, res) => {
+    try{
+        const director = await Director.findByPk(req.params.id);
+
+        if (!director) {
+            return res.status(404).json({ success: false, message: 'Director not found.' });
+        }
+
+        res.status(200).json(director);
+    } catch (error) {
+        handleErrorResponse(res, error, 'Error retrieving director');
+    }
+});
+
+app.post('/directors', async (req, res) => {
+    try {
+        const { name } = req.body;
+
+        const director = await Director.create({
+            name: name,
+        });
+
+        res.status(201).json(director);
+    } catch (error) {
+        handleErrorResponse(res, error, 'Error creating director');
+    }
+});
+
+app.put('/directors/:id', async (req, res) => {
+    const directorId = req.params.id;
+    const updatedData = req.body;
+
+    try {
+        const director = await Director.findByPk(directorId);
+
+        if (!director) {
+            return res.status(404).json({ success: false, message: 'Director not found.' });
+        }
+
+        await director.update(updatedData);
+
+        return res.status(200).json({ success: true, message: 'Director updated successfully.' });
+    } catch (error) {
+        handleErrorResponse(res, error, 'Error updating director');
+    }
+});
+
+app.delete('/directors/:id', async (req, res) => {
+    const directorId = req.params.id;
+
+    try {
+        const director = await Director.findByPk(directorId);
+        if (!director) {
+            return res.status(404).json({ success: false, message: 'Director not found.' });
+        }
+
+        await director.destroy();
+
+        return res.status(200).json({ success: true, message: 'Director deleted successfully.' });
+    } catch (error) {
+        handleErrorResponse(res, error, 'Error deleting director');
+    }
+});
+
+// Rute pentru filme
+app.get('/movies', async (req, res) => {
+    try {
+        const movies = await Movie.findAll({
+            include: [Director],
+        });
+
+        res.status(200).json(movies);
+    } catch (error) {
+        handleErrorResponse(res, error, 'Error retrieving movies');
+    }
+});
+
+app.get('/movies/:id', async (req, res) => {
+    try{
+        const movie = await Movie.findByPk(req.params.id, {
+            include: [Movie],
+        });
+
+        if (!movie) {
+            return res.status(404).json({ success: false, message: 'Movie not found.' });
+        }
+
+        res.status(200).json(movie);
+    } catch (error) {
+        handleErrorResponse(res, error, 'Error retrieving movie');
+    }
+});
+
+app.post('/movies', async (req, res) => {
+    try {
+        const { title, releaseDate, directorId } = req.body;
+        const director = await Director.findByPk(directorId);
+
+        if (!director) {
+            return res.status(404).json({ success: false, message: 'Director not found.' });
+        }
+
+        const movie = await Movie.create({
+            title: title,
+            releaseDate: releaseDate,
+            directorId: directorId
+        });
+
+        res.status(201).json(movie);
+    } catch (error) {
+        handleErrorResponse(res, error, 'Error creating movie');
+    }
+});
+
+app.put('/movies/:id', async (req, res) => {
+    const movieId = req.params.id;
+    const updatedData = req.body;
+
+    try {
+        const movie = await Movie.findByPk(movieId);
+
+        if (!movie) {
+            return res.status(404).json({ success: false, message: 'Movie not found.' });
+        }
+
+        await movie.update(updatedData);
+
+        return res.status(200).json({ success: true, message: 'Movie updated successfully.' });
+    } catch (error) {
+        handleErrorResponse(res, error, 'Error updating movie');
+    }
+});
+
+app.post('/favorite', async (req, res) => {
+    const { movieId, userId } = req.body;
+
+    try {
+        const movie = await Movie.findByPk(movieId);
+        const user = await User.findByPk(userId);
+
+        if (!movie) {
+            return res.status(404).json({ success: false, message: 'Movie not found.' });
+        }
+
+        if (!user) {
+            return res.status(404).json({ success: false, message: 'User not found.' });
+        }
+
+        await user.addMovie(movie);
+
+        return res.status(200).json({ success: true, message: 'Movie added.' });
+    } catch (error) {
+        handleErrorResponse(res, error, 'Error adding movie');
+    }
+});
+
+app.delete('/movies/:id', async (req, res) => {
+    const movieId = req.params.id;
+
+    try {
+        const movie = await Movie.findByPk(movieId);
+        if (!movie) {
+            return res.status(404).json({ success: false, message: 'Movie not found.' });
+        }
+
+        await movie.destroy();
+
+        return res.status(200).json({ success: true, message: 'Movie deleted successfully.' });
+    } catch (error) {
+        handleErrorResponse(res, error, 'Error deleting movie');
+    }
 });
 
 // Pornesc aplicatia backend pe portul 3001
